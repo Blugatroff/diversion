eac8a6b08f1dbb920f46a97cac4623d94b97b146
Jasper Reef <mail@jasperreef.com>
2023-12-04 13:13:03 +0100

    flake package

--- flake.nix
+++ flake.nix
@@ -1,32 +1,37 @@
 {
     inputs = {
         nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-        flake-utils.url = "github:numtide/flake-utils";
         rust-overlay = {
             url = "github:oxalica/rust-overlay";
-            inputs = {
-                nixpkgs.follows = "nixpkgs";
-                flake-utils.follows = "flake-utils";
-            };
+            inputs.nixpkgs.follows = "nixpkgs";
         };
     };
-    outputs = { self, nixpkgs, flake-utils, rust-overlay }:
-        flake-utils.lib.eachDefaultSystem
-            (system: let
-                overlays = [ (import rust-overlay) ];
-                pkgs = import nixpkgs {
-                    inherit system overlays;
+    outputs = { self, nixpkgs, rust-overlay }:
+        let
+            system = "x86_64-linux";
+            overlays = [ (import rust-overlay) ];
+            pkgs = import nixpkgs {
+                inherit system overlays;
+            };
+            nativeBuildInputs = with pkgs; [ 
+                pkgs.rust-bin.stable.latest.default 
+                pkg-config 
+            ];
+            buildInputs = [ pkgs.lua5_4 ];
+        in {
+            devShells.default = pkgs.mkShell {
+                inherit nativeBuildInputs buildInputs;
+            };
+            packages.x86_64-linux.default = pkgs.rustPlatform.buildRustPackage {
+                inherit nativeBuildInputs buildInputs;
+                pname = "diversion";
+                version = "1.0.0";
+                src = ./.;
+
+                cargoLock = {
+                    lockFile = ./Cargo.lock;
                 };
-                nativeBuildInputs = with pkgs; [ 
-                    pkgs.rust-bin.stable.latest.default 
-                    pkg-config 
-                    rust-analyzer
-                ];
-            in {
-                devShells.default = pkgs.mkShell {
-                    buildInputs = [ pkgs.lua5_4 ];
-                    inherit nativeBuildInputs;
-                };
-            });
+            };
+        };
 }
 
